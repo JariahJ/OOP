808b87bd8666a7ed9877d14b7269699a68217f4c
Jariah Jaramillo <jariahjaramillo@yahoo.com>
2019-04-16 15:02:35 -0600

    j

--- hw1/js/Hierarchy/Hierarchy/Hierarchy/building.js
+++ hw1/js/Hierarchy/Hierarchy/Hierarchy/building.js
@@ -18,6 +18,43 @@ class building {
 
     }
 }
+class house extends building {
+    constructor(color) {
+        super(color);
+    }
+    moveIn() {
+        if (this._finished = true) {
+            console.log("Move into house successful");
+        }
+        else {
+            console.log("Can't move into an unfinished house!");
+        }
+    }
+
+}
+
+class restaurant extends building{
+    constructor(color) {
+        super(color);
+    }
+    moveIn() {
+        if (this._finished = true) {
+            console.log("Successfully moved into restaurant!");
+        }
+        else {
+            console.log("Can't move into an unfinished restaurant");
+        }
+    }
+}
+
+var a = new house("BLACK");
+a.moveIn();
+a.color;
+var b = new restaurant("BROWN");
+b.moveIn();
+b.color;
+
+
 
 exports.building = building;
 
